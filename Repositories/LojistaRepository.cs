09af7e4a8bc86b991c9d9fb808d11946724c6d1b
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-09-22 15:58:37 -0300

    Estou triste com o código

---
+++ Repositories/LojistaRepository.cs
@@ -0,0 +1,19 @@
+﻿using CodeChallenge02.Models;
+using CodeChallenge02.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge02.Repositories
+{
+    public class LojistaRepository : ILojistaRepository
+    {
+        public Usuario buscarLojista(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult novoLojista(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/UsuarioComumRepository.cs
@@ -0,0 +1,46 @@
+﻿using CodeChallenge02.Database;
+using CodeChallenge02.Models;
+using CodeChallenge02.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge02.Repositories
+{
+    public class UsuarioComumRepository : IUsuarioComumRepository
+    {
+        private readonly PicPayContext picPayContext;
+        public UsuarioComumRepository(PicPayContext picPayContext)
+        {
+            this.picPayContext = picPayContext;
+        }
+
+        public Usuario buscarUsuario(int idUsuario)
+        {
+            try
+            {
+                var usuario = picPayContext.Usuarios.Where(u => u.Id == idUsuario).FirstOrDefault();
+
+                if (usuario == null)
+                    throw new Exception();
+
+                return usuario;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public IActionResult novoUsuario(Usuario usuario)
+        {
+            
+        }
+
+        public IActionResult Transferir(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
